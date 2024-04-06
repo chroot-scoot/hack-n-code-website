38945c67e0dcc9069d477bf971e24850f659ccab
pulkit4603 <pulkitdwivedi123@gmail.com>
2024-04-07 01:29:34 +0530

    style: auth page (without routing)

--- src/pages/authentication/components/LoginForm.tsx
+++ src/pages/authentication/components/LoginForm.tsx
@@ -1,9 +1,66 @@
-// LoginForm.tsx
 import React from 'react';
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  Card,
+} from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
+import { useAuthStore } from '../stores/authStore';
 
-const LoginForm: React.FC = () => {
-  // Add your login form implementation here
-  return <div>Login Form</div>;
-};
-
-export default LoginForm;
+export default function LoginForm() {
+  const { setIsUserFalse } = useAuthStore();
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl font-bold">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              placeholder="m@example.com"
+              required
+              type="email"
+            />
+          </div>
+          <div className="space-y-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link className="ml-auto inline-block text-sm underline" to="#">
+                Forgot your password?
+              </Link>
+            </div>
+            <Input
+              id="password"
+              placeholder="********"
+              required
+              type="password"
+            />
+          </div>
+          <Button className="w-full" type="submit">
+            Login
+          </Button>
+          <Button className="w-full" variant="outline">
+            Login with Google
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?
+          <Link className="underline" to="#" onClick={setIsUserFalse}>
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
